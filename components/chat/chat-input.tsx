8dc2567067c55bda4535a822da5cef77d3063904
stratosphereoff <77344536+stratosphereoff@users.noreply.github.com>
2024-05-31 14:50:15 +0300

    message file attachment

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -15,6 +15,7 @@ import {
 
 import { Input } from "@/components/ui/input";
 import { Plus, Smile } from "lucide-react";
+import { useModal } from "@/components/hooks/use-modal-store";
 
 interface ChatInputProps {
     apiUrl: string;
@@ -33,6 +34,8 @@ const ChatInput = ({
     name,
     type
 }: ChatInputProps) => {
+    const { onOpen } = useModal();
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -67,7 +70,7 @@ const ChatInput = ({
                                 <div className="relative p-4 pb-6">
                                     <button 
                                         type="button"
-                                        onClick={() => {}}
+                                        onClick={() => onOpen("messageFile", { apiUrl, query})}
                                         className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 
                                         hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 
                                         flex items-center justify-center"
--- components/hooks/use-modal-store.ts
+++ components/hooks/use-modal-store.ts
@@ -4,12 +4,15 @@ import { create} from "zustand";
 export type ModalType = 
 "createServer" | "invite" | "editServer" | 
 "members"| "createChannel" | "leaveServer" | 
-"deleteServer" | "deleteChannel" | "editChannel";
+"deleteServer" | "deleteChannel" | "editChannel" |
+"messageFile";
 
 interface ModalData {
     server?: Server;
     channel?: Channel;
     channelType?: ChannelType;
+    apiUrl?: string;
+    query?: Record<string, any>;
 }
 
 interface ModalStore {
