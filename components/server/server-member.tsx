164c7e2e7fc190444cbbfbfb0200f19b3b24d444
stratosphereoff <77344536+stratosphereoff@users.noreply.github.com>
2024-06-03 18:46:50 +0300

    server member fix

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -28,7 +28,7 @@ export const ServerMember = ({
     const icon = roleIconMap[member.role];
 
     const onClick = () => {
-        router.push(`/servers/${params.serverId}/conversations/${member.id}`);
+        router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
     }
 
     return(
